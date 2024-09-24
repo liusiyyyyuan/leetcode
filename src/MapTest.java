47b69ca6c9feee5f45b2d9bd19725146c8ceabb8
LiuSiyuan <1098381467@qq.com>
2024-09-24 09:14:21 +0800

    initial commit

---
+++ src/MapTest.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @version 1.0
+ * @Author liusiyuan
+ * @Data 2024/9/23 10:52
+ * @注释：
+ */
+public class MapTest {
+
+    LinkedHashMap<Integer, ArrayList<String>> map1;
+
+    public static void main(String[] args) {
+        MapTest mapTest = new MapTest();
+        Map<Integer, ArrayList<String>> map = mapTest.arrTest();
+    }
+
+    public LinkedHashMap<Integer, ArrayList<String>> arrTest() {
+        return this.map1;
+    }
+
+    public LinkedHashMap<Integer, ArrayList<String>> getMap1() {
+        return map1;
+    }
+
+    public void setMap1(LinkedHashMap<Integer, ArrayList<String>> map1) {
+        this.map1 = map1;
+    }
+}
